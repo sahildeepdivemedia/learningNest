aa09a8f97879402130962113ed4e85a32985f9d1
sahilbhaskar <sahil.bhaskar@deepdivemedia.in>
2024-12-26 15:59:44 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className={styles.shape}>
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -21,7 +22,9 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
+
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            sasasass
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
